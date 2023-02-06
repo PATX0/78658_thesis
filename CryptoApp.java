850cd2e6c2c241ffa7a20f45d2d39edfb92e7ba3
Tiago Soares <43506003+PATX0@users.noreply.github.com>
2023-02-06 00:29:41 +0000

    Create CryptoApp.java

---
+++ CryptoApp.java
@@ -0,0 +1,37 @@
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = findViewById(R.id.username_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        registerButton = findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                if (!username.isEmpty() && !password.isEmpty()) {
+                    // Perform registration logic here
+                    Toast.makeText(MainActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter username and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
